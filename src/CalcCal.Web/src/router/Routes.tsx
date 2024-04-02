e83ee4a2ab9a745f54f27359b608a51b4c51e344
DBrdak <brdak02dominik@gmail.com>
2024-04-02 22:54:43 +0200

    UserUI init

--- src/CalcCal.Web/src/router/Routes.tsx
+++ src/CalcCal.Web/src/router/Routes.tsx
@@ -1,12 +1,18 @@
 import { RouteObject } from "react-router"
 import App from "../app/App";
 import {createBrowserRouter} from "react-router-dom";
+import {UserPage} from "../app/user/UserPage";
+import {RegisterPage} from "../app/register/RegisterPage";
+import {LoginPage} from "../app/login/LoginPage";
 
 export const routes: RouteObject[] = [
     {
         path: '/',
         element: <App />,
         children: [
+            {path: '/user', element: <UserPage />},
+            {path: '/login', element: <LoginPage />},
+            {path: '/register', element: <RegisterPage />}
         ]
     }
 ]
